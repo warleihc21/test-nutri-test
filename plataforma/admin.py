d5e6bac196d42def55b6773aa357714f81b831d4
warleihc21 <warleihc21@gmail.com>
2022-07-13 21:09:50 -0300

    atualizacao

---
+++ plataforma/admin.py
@@ -0,0 +1,9 @@
+from doctest import DocTestParser
+from django.contrib import admin
+from .models import Pacientes, DadosPaciente, Refeicao, Opcao
+
+# Register your models here.
+admin.site.register(Pacientes)
+admin.site.register(DadosPaciente)
+admin.site.register(Refeicao)
+admin.site.register(Opcao)
